86ea955e4ef5d034fca0e6a44093a0de15408bcf
mikaeltaylor <mikael.e.taylor@gmail.com>
2016-05-16 07:54:00 -0500

    Slot Machine Commit

---
+++ SlotMachine/SlotMachine/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace SlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.spin = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.slotBox1 = new System.Windows.Forms.PictureBox();
+            this.slotBox2 = new System.Windows.Forms.PictureBox();
+            this.slotBox3 = new System.Windows.Forms.PictureBox();
+            this.amount = new System.Windows.Forms.TextBox();
+            this.amountLabel = new System.Windows.Forms.Label();
+            this.winnings = new System.Windows.Forms.Label();
+            this.totalWon = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.slotBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slotBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slotBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // spin
+            // 
+            this.spin.Location = new System.Drawing.Point(187, 255);
+            this.spin.Name = "spin";
+            this.spin.Size = new System.Drawing.Size(75, 23);
+            this.spin.TabIndex = 0;
+            this.spin.Text = "Spin";
+            this.spin.UseVisualStyleBackColor = true;
+            this.spin.Click += new System.EventHandler(this.spin_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(325, 255);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // slotBox1
+            // 
+            this.slotBox1.Image = global::SlotMachine.Properties.Resources.cherry;
+            this.slotBox1.Location = new System.Drawing.Point(37, 70);
+            this.slotBox1.Name = "slotBox1";
+            this.slotBox1.Size = new System.Drawing.Size(169, 125);
+            this.slotBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slotBox1.TabIndex = 2;
+            this.slotBox1.TabStop = false;
+            // 
+            // slotBox2
+            // 
+            this.slotBox2.Image = global::SlotMachine.Properties.Resources.orange;
+            this.slotBox2.Location = new System.Drawing.Point(212, 70);
+            this.slotBox2.Name = "slotBox2";
+            this.slotBox2.Size = new System.Drawing.Size(169, 125);
+            this.slotBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slotBox2.TabIndex = 3;
+            this.slotBox2.TabStop = false;
+            // 
+            // slotBox3
+            // 
+            this.slotBox3.Image = global::SlotMachine.Properties.Resources.strawberry;
+            this.slotBox3.Location = new System.Drawing.Point(388, 70);
+            this.slotBox3.Name = "slotBox3";
+            this.slotBox3.Size = new System.Drawing.Size(165, 125);
+            this.slotBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slotBox3.TabIndex = 4;
+            this.slotBox3.TabStop = false;
+            // 
+            // amount
+            // 
+            this.amount.Location = new System.Drawing.Point(325, 229);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(100, 20);
+            this.amount.TabIndex = 5;
+            this.amount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.amount_KeyPress);
+            // 
+            // amountLabel
+            // 
+            this.amountLabel.AutoSize = true;
+            this.amountLabel.Location = new System.Drawing.Point(197, 232);
+            this.amountLabel.Name = "amountLabel";
+            this.amountLabel.Size = new System.Drawing.Size(65, 13);
+            this.amountLabel.TabIndex = 6;
+            this.amountLabel.Text = "Amount Bet:";
+            // 
+            // winnings
+            // 
+            this.winnings.AutoSize = true;
+            this.winnings.Location = new System.Drawing.Point(247, 293);
+            this.winnings.Name = "winnings";
+            this.winnings.Size = new System.Drawing.Size(102, 13);
+            this.winnings.TabIndex = 7;
+            this.winnings.Text = "Amount Won: $0.00";
+            // 
+            // totalWon
+            // 
+            this.totalWon.AutoSize = true;
+            this.totalWon.Location = new System.Drawing.Point(232, 336);
+            this.totalWon.Name = "totalWon";
+            this.totalWon.Size = new System.Drawing.Size(129, 13);
+            this.totalWon.TabIndex = 8;
+            this.totalWon.Text = "Total Amount Won: $0.00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(306, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 358);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totalWon);
+            this.Controls.Add(this.winnings);
+            this.Controls.Add(this.amountLabel);
+            this.Controls.Add(this.amount);
+            this.Controls.Add(this.slotBox3);
+            this.Controls.Add(this.slotBox2);
+            this.Controls.Add(this.slotBox1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.spin);
+            this.Name = "Form1";
+            this.Text = "Slot Machine v1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.slotBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slotBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slotBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button spin;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.PictureBox slotBox1;
+        private System.Windows.Forms.PictureBox slotBox2;
+        private System.Windows.Forms.PictureBox slotBox3;
+        private System.Windows.Forms.TextBox amount;
+        private System.Windows.Forms.Label amountLabel;
+        private System.Windows.Forms.Label winnings;
+        private System.Windows.Forms.Label totalWon;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SlotMachine/SlotMachine/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlotMachine
+{
+    public partial class Form1 : Form
+    {
+        string slotValue1;
+        string slotValue2;
+        string slotValue3;
+        double totalWinnings = 0;
+        double previousWinnings= 0;
+        double totalBets = 0;
+        double won = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static readonly Random slotRandomizer = new Random();
+        private int[] slotNumbers;
+
+        private void spin_Click(object sender, EventArgs e)
+        { if (amount.Text != ""){
+            slotNumbers = new int[3];
+            slotNumbers[0] = slotRandomizer.Next(0, 100);
+            slotNumbers[1] = slotRandomizer.Next(0, 100);
+            slotNumbers[2] = slotRandomizer.Next(0, 100);
+
+            if (slotNumbers[0] <= 33 && slotNumbers[0] >= 0)
+            {
+                slotBox1.Image = Properties.Resources.cherry;
+                slotValue1 = "cherry";
+                slotBox1.Refresh();
+                slotBox1.Visible = true;
+            }
+            else if (slotNumbers[0] >= 34 && slotNumbers[0] <= 66)
+            {
+                slotBox1.Image = Properties.Resources.orange;
+                slotValue1 = "orange";
+                slotBox1.Refresh();
+                slotBox1.Visible = true;
+            }
+            else if (slotNumbers[0] >= 67 && slotNumbers[0] <= 100)
+            {
+                slotBox1.Image = Properties.Resources.strawberry;
+                slotValue1 = "strawberry";
+                slotBox1.Refresh();
+                slotBox1.Visible = true;
+            }
+
+            if (slotNumbers[1] <= 33 && slotNumbers[1] >= 0)
+            {
+                slotBox2.Image = Properties.Resources.cherry;
+                slotValue2 = "cherry";
+                slotBox2.Refresh();
+                slotBox2.Visible = true;
+            }
+            else if (slotNumbers[1] >= 34 && slotNumbers[1] <= 66)
+            {
+                slotBox2.Image = Properties.Resources.orange;
+                slotValue2 = "orange";
+                slotBox2.Refresh();
+                slotBox2.Visible = true;
+            }
+            else if (slotNumbers[1] >= 67 && slotNumbers[1] <= 100)
+            {
+                slotBox2.Image = Properties.Resources.strawberry;
+                slotValue2 = "strawberry";
+                slotBox2.Refresh();
+                slotBox2.Visible = true;
+            }
+            //Third slot box.  Figure out an image and a value to pop up based off of random number generation used above.
+            if (slotNumbers[2] <= 33 && slotNumbers[2] >= 0)
+            {
+                slotBox3.Image = Properties.Resources.cherry;
+                slotValue3 = "cherry";
+                slotBox3.Refresh();
+                slotBox3.Visible = true;
+            }
+            else if (slotNumbers[2] >= 34 && slotNumbers[2] <= 67)
+            {
+                slotBox3.Image = Properties.Resources.orange;
+                slotValue3 = "orange";
+                slotBox3.Refresh();
+                slotBox3.Visible = true;
+            }
+            else if (slotNumbers[2] >= 68 && slotNumbers[2] <= 100)
+            {
+                slotBox3.Image = Properties.Resources.strawberry;
+                slotValue3 = "strawberry";
+                slotBox3.Refresh();
+                slotBox3.Visible = true;
+            }
+            //Now, calculate the amount they've won, if any.  If they did not win, subtract the amount from total winnings.
+            //If two of the values match, they win double their money, if it's three of the values that match, then they win 3x their money & if it's 1 or none, they lose their bet.
+            double betAmount = Convert.ToDouble(amount.Text);
+            totalBets = totalBets + betAmount;
+            if (slotValue1 == "orange" && slotValue2 == "orange" && slotValue3 != "orange" || slotValue2 == "orange" && slotValue3 == "orange" && slotValue1 != "orange" || slotValue1 == "orange" && slotValue3 == "orange" && slotValue2 != "orange")
+            {
+                previousWinnings = totalWinnings;
+                won = betAmount * 2;
+                totalWinnings = won + previousWinnings;
+                winnings.Text = "Amount Won: $" + won.ToString("N");
+                totalWon.Text = "Total Amount Won: $"+ totalWinnings.ToString("N");
+            }
+            else if (slotValue1 == "strawberry" && slotValue2 == "strawberry" && slotValue3 != "strawberry" || slotValue2 == "strawberry" && slotValue3 == "strawberry" && slotValue1 != "strawberry" || slotValue1 == "strawberry" && slotValue3 == "strawberry" && slotValue2 != "strawberry")
+            {
+                previousWinnings = totalWinnings;
+                won = betAmount * 2;
+                totalWinnings = won + previousWinnings;
+                winnings.Text = "Amount Won: $" + won.ToString("N");
+                totalWon.Text = "Total Amount Won: $" + totalWinnings.ToString("N");
+            }
+            else if (slotValue1 == "cherry" && slotValue2 == "cherry" && slotValue3 != "cherry" || slotValue2 == "cherry" && slotValue3 == "cherry" && slotValue1 != "cherry" || slotValue1 == "cherry" && slotValue3 == "cherry" && slotValue2 != "cherry")
+            {
+                previousWinnings = totalWinnings;
+                won = betAmount * 2;
+                totalWinnings = won + previousWinnings;
+                winnings.Text = "Amount Won: $" + won.ToString("N");
+                totalWon.Text = "Total Amount Won: $" + totalWinnings.ToString("N");
+            }
+            else if (slotValue1 == "orange" && slotValue2 == "orange" && slotValue3 == "orange" || slotValue2 == "orange" && slotValue3 == "orange" && slotValue1 == "orange" || slotValue1 == "orange" && slotValue3 == "orange" && slotValue2 == "orange")
+            {
+                previousWinnings = totalWinnings;
+                won = betAmount * 3;
+                totalWinnings = won + previousWinnings;
+                winnings.Text = "Amount Won: $" + won.ToString("N");
+                totalWon.Text = "Total Amount Won: $" + totalWinnings.ToString("N");
+            }
+            else if (slotValue1 == "strawberry" && slotValue2 == "strawberry" && slotValue3 == "strawberry" || slotValue2 == "strawberry" && slotValue3 == "strawberry" && slotValue1 == "strawberry" || slotValue1 == "strawberry" && slotValue3 == "strawberry" && slotValue2 == "strawberry")
+            {
+                previousWinnings = totalWinnings;
+                won = betAmount * 3;
+                totalWinnings = won + previousWinnings;
+                winnings.Text = "Amount Won: $" + won.ToString("N");
+                totalWon.Text = "Total Amount Won: $" + totalWinnings.ToString("N");
+            }
+            else if (slotValue1 == "cherry" && slotValue2 == "cherry" && slotValue3 == "cherry" || slotValue2 == "cherry" && slotValue3 == "cherry" && slotValue1 == "cherry" || slotValue1 == "cherry" && slotValue3 == "cherry" && slotValue2 == "cherry")
+            {
+                previousWinnings = totalWinnings;
+                won = betAmount * 3;
+                totalWinnings = won + previousWinnings;
+                winnings.Text = "Amount Won: $" + won.ToString("N");
+                totalWon.Text = "Total Amount Won: $" + totalWinnings.ToString("N");
+            }
+            else
+            {
+                previousWinnings = totalWinnings -betAmount;
+                won = 0- betAmount;
+                totalWinnings = totalWinnings + won;
+                winnings.Text = "Amount Won: -$" + betAmount.ToString("N");
+                totalWon.Text = "Total Amount Won: $" + totalWinnings.ToString("N");
+            }
+        }
+            else if (amount.Text == "")
+            {
+                MessageBox.Show("Please enter a valid bet amount.");
+        }
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            //If they click exit, then show them what they've won/bet and ask if they really want to quit.
+            DialogResult result = MessageBox.Show("Thanks for Playing! You've won $"+Convert.ToString(totalWinnings)+" and you've bet a total amount of $"+Convert.ToString(totalBets)+". Are you sure you want to quit?", "Exit?", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void amount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //keypress validation.  Only allow numeric entries for the bet amount.
+            if (e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                MessageBox.Show("Please enter a valid numerical amount for your bet.  Please do not include the $ sign.");
+            }
+        }
+    }
+}
